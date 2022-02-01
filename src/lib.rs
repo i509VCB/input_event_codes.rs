494f5983de8a3d4467eef6cd9dd3736ff2db1bde
i509VCB <git@i509.me>
2022-02-01 13:36:52 -0600

    crate level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! # Input event codes
+//!
+//! This library provides constants representing the input event codes generated from `input-event-codes.h` in
+//! the Linux kernel.
+//!
+//! Some other operating systems may also use the same input codes as Linux, namely FreeBSD.
+//!
+//! These input codes may be used in conjunction with kernel apis to read input events or with a higher level library
+//! such as [libinput](https://crates.io/crates/input).
+
 #![no_std]
 
 mod generated;
